165dd6c98e1516a6df0cff25f28873b656d95add
MohamedOsoble <Mohamed.Osoble@outlook.com>
2024-10-29 18:56:37 +0000

    feat: write test and function for Battleship.isSunk();

--- main.test.js
+++ main.test.js
@@ -1,7 +1,13 @@
 import { Battleship } from "./main";
 
-const shipOne = new Battleship(5);
 test('Counter for ship hits increases hit counter', () => {
+    const shipOne = new Battleship(5);
     shipOne.hit();
     expect(shipOne.hits).toBe(1);
+});
+
+test('If Battleship.length is equal to Battleship.hits, Battleship.sunk should be true', () => {
+    const shipOne = new Battleship(1);
+    shipOne.hit();
+    expect(shipOne.isSunk()).toBe(true);
 });
